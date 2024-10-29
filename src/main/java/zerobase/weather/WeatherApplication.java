318787ff600c7f04ecad0e0e4e0938c5db34f176
shCHO9801 <csh980116@naver.com>
2024-10-29 16:26:46 +0900

    JDBC 방식으로 데이터 저장하기

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -2,8 +2,10 @@ package zerobase.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"zerobase.weather", "repository"})
 public class WeatherApplication {
 
 	public static void main(String[] args) {
