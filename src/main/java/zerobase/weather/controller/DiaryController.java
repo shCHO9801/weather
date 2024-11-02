572f97a8647300fecff350f3177d32c1e3ad1ca6
shCHO9801 <csh980116@naver.com>
2024-11-02 18:58:55 +0900

    Chapter 10. API document 만들기

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,14 +1,19 @@
 package zerobase.weather.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weather.domain.Diary;
-import zerobase.weather.error.InvalidDate;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "diary-controller", description = "다이어리 API")
 @RestController
 public class DiaryController {
     private final DiaryService diaryService;
@@ -17,6 +22,8 @@ public class DiaryController {
         this.diaryService = diaryService;
     }
 
+
+    @Operation(summary = "여기에 말을 치면 어디에 뜰까?", description = "이것은 노트")
     @PostMapping("/create/diary")
     void createDiary(
             @RequestParam
@@ -26,6 +33,7 @@ public class DiaryController {
         diaryService.createDiary(date, text);
     }
 
+    @Operation(summary = "선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     List<Diary> readDiary(
             @RequestParam
@@ -37,13 +45,23 @@ public class DiaryController {
         return diaryService.readDiary(date);
     }
 
+    @Operation(summary = "선택한 기간중의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
+
     List<Diary> readDiaries(
+            @Parameter(
+                    description = "조회할 기간의 첫번째날",
+                    example = "2022-02-02"
+            )
             @RequestParam
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @Parameter(
+                    description = "조회할 기간의 마지막 날",
+                    example = "2022-02-02"
+            )
             @RequestParam
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
-    ){
+    ) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
